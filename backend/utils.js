258fd97744109f0ce921c9d4cc27b00f581d7cff
Jessica Leverett <Jessak1202@gmail.com>
2021-03-29 11:37:48 -0500

    PlaceOrder API

--- backend/utils.js
+++ backend/utils.js
@@ -10,7 +10,24 @@ export const generateToken = (user) => {
      process.env.JWT_SECRET || 'somethingsecret',
       {
         expiresIn: '30d', 
-
       }
     );
 };
+
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(authorization){
+    const token = authorization.slice(7, authorization.length); // Bearer XXXXXX - GETS RIDE OF BEARER APART AND RETURNS TOKEN PORTION 
+    jwt.verify(token, process.env.JWT_SECRET || 'somethingsecret', (err, decode) => {
+      if(err) {
+        res.status(401).send({message: 'Invalid Token! '});
+      } else {
+        // at this point token is correct
+        req.user = decode;
+      }
+    }
+   ) ;
+  } else {
+    res.status(401).send({ message: 'No Token! '});
+  }
+}
